caa3ba420dc55cab529d0c5c221cb30b02d10730
ezestom <ezequielstom@gmail.com>
2023-03-24 19:15:49 -0300

    add

---
+++ src/script.js
@@ -0,0 +1,60 @@
+// const favicon = document.querySelector('link[rel="icon"]')
+
+// document.addEventListener("visibilitychange", () => {
+//     const hidden = document.hidden
+
+//     favicon.setAttribute(
+//         "href", 
+//         `/favicon${hidden ? "-hidden" : ""}.ico`
+//     )
+// })
+
+let previousTitle = document.title;
+
+window.addEventListener("blur", () => {
+	previousTitle = document.title;
+	document.title = "Don't go away, come back!";
+});
+window.addEventListener("focus", () => {
+	document.title = previousTitle;
+});
+
+// 
+
+const overlay = document.querySelector("[data-overlay]");
+const navbar = document.querySelector("[data-navbar]");
+const menuCloseBtn = document.querySelector("[data-nav-close-btn]");
+const menuOpenBtn = document.querySelector("[data-nav-open-btn]");
+
+const elemArr = [overlay, menuCloseBtn, menuOpenBtn];
+
+for (let i = 0; i < elemArr.length; i++) {
+	elemArr[i].addEventListener("click", function () {
+		navbar.classList.toggle("active");
+		overlay.classList.toggle("active");
+	});
+}
+
+/**
+ * toggle navbar when any navbar link will be clicked
+ */
+
+const navbarLinks = document.querySelectorAll("[data-navlink]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+	navbarLinks[i].addEventListener("click", function () {
+		navbar.classList.toggle("active");
+		overlay.classList.toggle("active");
+	});
+}
+
+const backBtn = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+	backBtn.style.display = "fixed";
+});
+
+
+
+
+
