37942b7fd29ef458303cc761f451956cb47eacd8
ezequiel stom <ezequielstom@gmail.com>
2024-09-02 11:43:45 -0300

    chore: Update npm dependency for Toastify.js library

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,5 +1,4 @@
 emailjs.init("cmbu9tVx0xBzhilTM");
-const btn = document.querySelector(".send-button");
 
 document
 	.querySelector("#message-form")
@@ -15,12 +14,33 @@ document
 			from_email: email,
 			message: message,
 		};
+
 		emailjs
 			.send("service_fnc8jal", "template_htwlbzx", templateParams)
 			.then((response) => {
-				alert("Your message has been sent!");
+				// alert("Your message has been sent!");
+				Toastify({
+					text: "Your message has been sent! ✅",
+					className: "info",
+					style: {
+						background:
+							"linear-gradient(to right, #00b09b, #96c93d)",
+					},
+				}).showToast();
+				// Limpiar los campos de entrada
+				document.querySelector(".name-input").value = "";
+				document.querySelector(".email-input").value = "";
+				document.querySelector(".message-input").value = "";
 			})
 			.catch((error) => {
-				alert("Something went wrong..." + error);
+				// alert("Something went wrong... " + error);
+				Toastify({
+					text: "Something went wrong! ❌ Please try again later.",
+					className: "info",
+					style: {
+						background:
+							"linear-gradient(to right, #ff416c, #ff4b2b)",
+					},
+				}).showToast();
 			});
 	});
